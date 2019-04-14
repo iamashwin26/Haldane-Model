e2f9e4351785c55565a53f95b53a2b60e2501259
penji2 <krystian.pegowski@gmail.com>
2019-04-14 20:29:29 +0200

    fixed 3d plot

--- Haldane.py
+++ Haldane.py
@@ -8,7 +8,7 @@ from mpl_toolkits.mplot3d import Axes3D
 class Haldane:
     def __init__(self):
         self.t = 1
-        self.lamb = 0.3
+        self.lamb = 0
         self.V = 0
         self.a1 = 0.5 * np.array([-3 ** 0.5, 3])  # translation vector1 in position domain
         self.a2 = 0.5 * np.array([3 ** 0.5, 3])  # translation vector2 in position domain
@@ -27,10 +27,9 @@ class Haldane:
         return hamiltonian
 
     def get_eigenvalues_for_plot(self):
-        eigenvalues_array_0, eigenvalues_array_1, kx_array = list(), list(), list()
+        eigenvalues_array_0, eigenvalues_array_1, kx_array, ky_array = list(), list(), list(), list()
         ky = -np.pi
-        k_step = 2
-
+        k_step = 0.1
         for kx in np.arange(-np.pi, np.pi, k_step):  # defining grid loop
             k1 = np.dot(np.array([kx, ky]), self.a1)
             for ky in np.arange(-np.pi, np.pi, k_step):
@@ -39,18 +38,18 @@ class Haldane:
                 eigenvalues_array_0.append(np.linalg.eigh(self.define_hamiltonian(k1, k2))[0][0])
                 eigenvalues_array_1.append(np.linalg.eigh(self.define_hamiltonian(k1, k2))[0][1])
                 kx_array.append(kx)
+                ky_array.append(ky)
 
-        return kx_array, eigenvalues_array_0, eigenvalues_array_1
+        return kx_array, ky_array, eigenvalues_array_0, eigenvalues_array_1
 
     def draw_plot(self):
-        x, y, z = self.get_eigenvalues_for_plot()
+        x, y, z1, z2 = self.get_eigenvalues_for_plot()
         # x, y = np.meshgrid(x, y)
-        print('co to Z', z)
-        print('co to Y', y)
-        print('co to X', x)
+
         ax = plt.axes(projection='3d')
 
-        ax.plot_trisurf(x, y, z)
+        ax.plot_trisurf(x, y, z1)
+        ax.plot_trisurf(x, y, z2)
         plt.show()
 
 
