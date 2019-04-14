58a67183a725a99f1f61429ea66c1f38e205aa1e
penji2 <krystian.pegowski@gmail.com>
2019-04-14 16:37:27 +0200

    added plot3d

--- Haldane.py
+++ Haldane.py
@@ -6,11 +6,12 @@ from mpl_toolkits.mplot3d import Axes3D
 
 
 t = 1
-lamb = 0
-V = 1
+lamb = 0.3
+V = 0
+a1 = 0.5 * np.array([-3 ** 0.5, 3])  # translation vector1 in position domain
+a2 = 0.5 * np.array([3 ** 0.5, 3])  # translation vector2 in position domain
 
 """hamiltonian for a given k"""
-
 
 def define_hamiltonian(k1, k2):
     global t
@@ -28,21 +29,16 @@ def define_hamiltonian(k1, k2):
 
 
 def get_eigenvalues_for_plot():
-
-    a1 = 0.5 * np.array([-3**0.5, 3])
-    a2 = 0.5 * np.array([3**0.5, 3])
-
+    global a1
+    global a2
     eigenvalues_array_0 = []
     eigenvalues_array_1 = []
-
     kx_array = []
     ky_array = []
-
-    kx = -np.pi
     ky = -np.pi
-    k_step = 0.01
+    k_step = 2
 
-    for kx in np.arange(-np.pi, np.pi, k_step):
+    for kx in np.arange(-np.pi, np.pi, k_step): # defining grid loop
         k1 = np.dot(np.array([kx, ky]), a1)
         for ky in np.arange(-np.pi, np.pi, k_step):
             k2 = np.dot(np.array([kx, ky]), a2)
@@ -52,31 +48,67 @@ def get_eigenvalues_for_plot():
 
             ky_array.append(ky)
             kx_array.append(kx)
+    return kx_array, ky_array, eigenvalues_array_0, eigenvalues_array_1, energy2d_array
 
-    return kx_array, ky_array, eigenvalues_array_0, eigenvalues_array_1
 
-
-def plot3d(X, Y, z1, z2):
-
-    # x, y = np.meshgrid(X, Y)
-    # print(X)
-    # print(Y)
-    # print(z1)
-    # print(z2)
+def plot3d(x, y, z1, z2):
 
     fig = plt.figure()
     ax = plt.axes(projection='3d')
-    print(len(Y), len(X))
 
-    ax.scatter3D(X, Y, z2)
+    ax.scatter3D(x, y, z2)
+    ax.scatter3D(x, y, z1)
+
     ax.set_xlabel('$\k_x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
     plt.show()
 
 
+def plot_testing(x, y, z1, z2, Z):
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    X, Y = np.meshgrid(x, y)
+    print(X)
+    ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
+                    cmap='viridis', edgecolor='none')
+    plt.show()
+
+
+def f(x, y):
+    return x + y
+
+
+def test():
+
+    x = np.linspace(-6, 6, 3)
+    y = np.linspace(-6, 6, 3)
+
+    X, Y = np.meshgrid(x, y)
+    Z = f(X, Y)
+    print('co to Z', Z)
+    print('co to Y', Y)
+    print('co to X', X)
+    ax = plt.axes(projection='3d')
+    ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
+                    cmap='viridis', edgecolor='none')
+    plt.show()
+
+def make_board(BOARD_LENGTH):
+    spin_array2D = []
+    for i in range(BOARD_LENGTH+1):
+        spin_array2D_rows= []
+        for j in range(BOARD_LENGTH+1):
+            spin = random.randint(0, 1)
+            if spin == 0:
+                spin =- 1
+            spin_array2D_rows.append(spin)
+        spin_array2D.append(spin_array2D_rows)
+    return (spin_array2D)
+
 def main():
-    plot3d(get_eigenvalues_for_plot()[0], get_eigenvalues_for_plot()[1], get_eigenvalues_for_plot()[2], get_eigenvalues_for_plot()[3])
+    # plot_testing(get_eigenvalues_for_plot()[0], get_eigenvalues_for_plot()[1], get_eigenvalues_for_plot()[2], get_eigenvalues_for_plot()[3], get_eigenvalues_for_plot()[4])
+    test()
 
 
 main()
