90a02f64931f3d5acd9d6b8a6ae3c33b4fbd11ff
penji2 <krystian.pegowski@gmail.com>
2019-04-15 11:12:00 +0200

    plotting 3d is fine, added scratch functions for getting energy for path, and for plotting that energy-path

--- Haldane.py
+++ Haldane.py
@@ -20,7 +20,7 @@ class Haldane:
         ba = np.conj(ab)
         aa = self.V + 1j * self.lamb * (-np.exp(1j * (k1 - k2)) + np.exp(1j * k1) - np.exp(1j * k2)
                                         + np.exp(-1j * (k1 - k2)) - np.exp(-1j * k1) + np.exp(-1j * k2))
-        bb = -np.conj(aa)
+        bb = np.conj(aa - self.V) - self.V
 
         hamiltonian = np.array([[aa, ab],
                                 [ba, bb]])
@@ -29,12 +29,11 @@ class Haldane:
     def get_eigenvalues_for_plot(self):
         eigenvalues_array_0, eigenvalues_array_1, kx_array, ky_array = list(), list(), list(), list()
         ky = -np.pi
-        k_step = 0.1
+        k_step = 0.05
         for kx in np.arange(-np.pi, np.pi, k_step):  # defining grid loop
-            k1 = np.dot(np.array([kx, ky]), self.a1)
             for ky in np.arange(-np.pi, np.pi, k_step):
+                k1 = np.dot(np.array([kx, ky]), self.a1)
                 k2 = np.dot(np.array([kx, ky]), self.a2)
-
                 eigenvalues_array_0.append(np.linalg.eigh(self.define_hamiltonian(k1, k2))[0][0])
                 eigenvalues_array_1.append(np.linalg.eigh(self.define_hamiltonian(k1, k2))[0][1])
                 kx_array.append(kx)
@@ -42,28 +41,50 @@ class Haldane:
 
         return kx_array, ky_array, eigenvalues_array_0, eigenvalues_array_1
 
-    def draw_plot(self):
+    def get_eigenvalues_for_plot_path(self):
+        eigenvalues_array_0, eigenvalues_array_1, kx_array, ky_array = list(), list(), list(), list()
+        ky = -np.pi
+        k_step = 0.05
+        for kx in np.arange(-np.pi, np.pi, k_step):  # defining grid loop
+            for ky in np.arange(-np.pi, np.pi, k_step):
+                k1 = np.dot(np.array([kx, ky]), self.a1)
+                k2 = np.dot(np.array([kx, ky]), self.a2)
+                eigenvalues_array_0.append(np.linalg.eigh(self.define_hamiltonian(k1, k2))[0][0])
+                eigenvalues_array_1.append(np.linalg.eigh(self.define_hamiltonian(k1, k2))[0][1])
+                kx_array.append(kx)
+                ky_array.append(ky)
+
+        return kx_array, ky_array, eigenvalues_array_0, eigenvalues_array_1
+
+    def draw_energy_plot3d(self):
+        label_font_size = 20
         x, y, z1, z2 = self.get_eigenvalues_for_plot()
-        #x, y = np.meshgrid(x, y)
 
         ax = plt.axes(projection='3d')
 
-        ax.plot_trisurf(x, y, z1)
-        ax.plot_trisurf(x, y, z2)
+        ax.scatter3D(x, y, z1, c=z1, cmap='rainbow')
+        ax.scatter3D(x, y, z2, c=z2, cmap='rainbow')
 
-        # z1 = np.tile(z1, (len(z1), 1))
-        # z2 = np.tile(z2, (len(z2), 1))
-        #
-        # ax.plot_surface(x, y, z1)
-        # ax.plot_surface(x, y, z2)
-
+        plt.title("Energy bands in 3D", fontsize=label_font_size)
+        ax.set_xlabel("$k_x$", fontsize=label_font_size)
+        ax.set_ylabel("$k_y$", fontsize=label_font_size)
+        ax.set_zlabel("E", fontsize=label_font_size)
         plt.show()
 
+    def draw_energy_plot_path(self):
+        label_font_size = 20
+        x, y1, y2 = self.get_eigenvalues_for_plot_path()
+
+        ax = plt.axes(projection='3d')
+
+        plt.plot(x, y1, 'o', markersize=3, color='red')
+        plt.plot(x, y2, 'o', markersize=3, color='red')
+
+        plt.title("Energy path in brillouin zone", fontsize=label_font_size)
+        ax.set_xlabel("$k_x$", fontsize=label_font_size)
+        ax.set_ylabel("E", fontsize=label_font_size)
+        plt.show()
 
 if __name__ == '__main__':
-    # test = Haldane()
-    # test.draw_plot()
-
-    x = 1j*3
-    y = np.conj(x)
-    print(x, y)
+    test = Haldane()
+    test.draw_plot3d()
